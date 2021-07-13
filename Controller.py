d1b6c2e50ebc79675100b677c86a8ddf40f6d985
Felix <F.Semler@student.rug.nl>
2021-07-13 07:20:46 -0700

    basic windows application interaction

--- Controller.py
+++ Controller.py
@@ -5,6 +5,7 @@ Interacting with the windows software, must be started from python script to wor
 from pywinauto.application import Application #High level API to interact with windows application using Win32 API and MS UI Automation
 from pywinauto.findwindows import ElementNotFoundError
 from Helper_funcs import dprint
+import os
 
 class GUI_interaction:
     def __init__(self): #TODO: Time init after cpmpletion so that all can be launched prior to sun down
@@ -14,25 +15,17 @@ class GUI_interaction:
         self.apps = {} #Dictionary containing all window interaction objects
 
         #           Set up AAG CloudWatcher ---> weather station
-        try:   #TODO: check if flag: allow_magic_lookup=False should be added due to ambigiosity
-            app = Application(backend="uia").start('C:\Program Files (x86)\AAG_CloudWatcher.exe') #TODO: Check which backends to use
-        except:
-            app = Application(backend="win32").start('C:\Program Files (x86)\AAG_CloudWatcher.exe') #TODO: Check which backends to use
-            dprint('used win32')
-        dlg_spec = app.AAG_CloudWatcherMASTER #TODO: maybe this is correct?
-        actionable_dlg = dlg_spec.wait('visible')
-        #Get Detailed Window specification:
-        dlg_spec = app.window(title='AAG_CloudWatcher MASTER (v8.01.000)')
         try:
-            dlg_spec.wrapper_object()
-        except ElementNotFoundError as e:
-            dprint('Title of application probably wrong')
-        # To get possible control sequences print_control_identifiers()
-        dlg_spec.Start.clock() #TODO: check ths starts
-        #Assign window to dictionary for later usage
+            app = Application(backend="uia").start('C:\Program Files (x86)\AAG_CloudWatcher\AAG_CloudWatcher.exe')
+            app.AAG_CloudWatcher.START2.click()
+        except:
+            dprint('Restarting AAG_CloudWatcher.exe')
+            os.system("TASKKILL /F /IM AAG_CloudWatcher.exe")
+            app = Application(backend="uia").start('C:\Program Files (x86)\AAG_CloudWatcher\AAG_CloudWatcher.exe')
+            app.AAG_CloudWatcher.START2.click()
         self.apps['AAG'] = dlg_spec
 
-        #       Set up ScopeDome ---> DomeControl
+        #       Set up ScopeDome ---> DomeControl #TODO:
         try:   #TODO: check if flag: allow_magic_lookup=False should be added due to ambigiosity
             app = Application(backend="uia").start('C:\ScopeDome\Driver_LS\ASCOM.ScopeDomeUSBDome.exe') #TODO: Check which backends to use
         except:
@@ -49,7 +42,7 @@ class GUI_interaction:
         self.apps['ScopeDome'] = new_window
 
 
-        #       Set up 10micron virtual keypad ---> mount control
+        #       Set up 10micron virtual keypad ---> mount control #TODO:
         try:   #TODO: check if flag: allow_magic_lookup=False should be added due to ambigiosity
             app = Application(backend="uia").start('C:\Program Files (x86)\10micron\VirtKP2\virtkeypad.exe') #TODO: Check which backends to use
         except:
@@ -59,22 +52,31 @@ class GUI_interaction:
         self.apps['virtkeypad'] = app
 
         #       Set up MaximDL ---> CCD control
-        try:   #TODO: check if flag: allow_magic_lookup=False should be added due to ambigiosity
-            app = Application(backend="uia").start('C:\Program Files (x86)\Diffraction Limited\MaxIm DL 6\MaxIm_DL') #TODO: Check which backends to use
+        try:
+            app = Application(backend="uia").start('C:\Program Files (x86)\Diffraction Limited\MaxIm DL 6\MaxIm_DL.exe')
         except:
-            app = Application(backend="win32").start('C:\Program Files (x86)\Diffraction Limited\MaxIm DL 6\MaxIm_DL') #TODO: Check which backends to use
-            dprint('used win32')
-        #TODO: Do unpacking, currently license issues
+            dprint('Restarting MaxIm_DL.exe')
+            os.system("TASKKILL /F /IM MaxIm_DL.exe")
+            app = Application(backend="uia").start('C:\Program Files (x86)\Diffraction Limited\MaxIm DL 6\MaxIm_DL.exe')
+
+        #TODO: License issues, finish set up
 
         #       Set up ACP Planer ---> Scheduler
-        try:   #TODO: check if flag: allow_magic_lookup=False should be added due to ambigiosity
-            app = Application(backend="uia").start('C:\Program Files (x86)\Diffraction Limited\MaxIm DL 6\MaxIm_DL') #TODO: Check which backends to use
-        except: #TODO: Fix location, cant get past shortcut for some reason
-            app = Application(backend="win32").start('C:\Program Files (x86)\Diffraction Limited\MaxIm DL 6\MaxIm_DL') #TODO: Check which backends to use
-            dprint('used win32')
+        try:
+            app = Application(backend="uia").start('C:\Program Files (x86)\ACP Obs Control\Planner\ACPPlanGen.exe')
+        except:
+            dprint('Restarting ACP Planner.exe')
+            os.system("TASKKILL /F /IM ACPPlanGen.exe")
+            app = Application(backend="uia").start('C:\Program Files (x86)\ACP Obs Control\Planner\ACPPlanGen.exe')
+
+        app['Dialog'].OK.click()
+        app['ACP Planner Tips'].CLOSE.click()
+        main_win = app['Create Plan for My Observatory']
+        self.apps['ACP'] = main_win
 
     def check_processes(self):
         """Monitor weather data and scheduler
         IMPORTANT: This should be running in parallel to CCD control and new scheduler so write independently
         """ #TODO: change bios to auto boot, get some random pc for wake on lan?
         raise Exception('Not Implemented yet')
+
