bc3ae5e1a7bdb618c0fa5599ac3cf15df7657d2a
Naderschah <F.Semler@student.rug.nl>
2021-08-13 14:22:57 +0200

    commit

--- Background_Script.py
+++ Background_Script.py
@@ -35,14 +35,15 @@ logger.addHandler(file_handler)
 logger.setLevel(logging.DEBUG)
 time.sleep(2.0)
 logger.debug('Logger is working')
-
+passes = 0
 #Set starting time for first execution
 if __name__ == '__main__':
     while True:
         try:
             weather_pred = get_predicted_conditions(short = True) #Only retrieves sunset time
             logger.info('Sunset time retrieved as {}'.format(weather_pred['Sunset']..strftime('%H:%M:%S')))
-            #Start script 1/2-one hour before sunset - FIXME: What about astronomical sunset?
+
+            #Start script half to one hour before sunset - FIXME: What about astronomical sunset?
             while weather_pred['Sunset']-dt.timedelta(hours=1)<dt.datetime.now():
                 time.sleep(1800) #wait 30 min
             logger.info('Starting pipe')
@@ -52,12 +53,15 @@ if __name__ == '__main__':
             keyboard = Controller()
             keyboard.press('`') 
             logger.info('Triggered AHK script')
-            #Load the SQLite database
+
+            #Load the SQLite database FIXME: Figure out a way to write to database
             objects = daily_Schedule(os.path.join(file_path,'Database.db'), 'Schedule')
             logger.info('Loaded database')
+
             #Create the schedule for the night
             objects, schedule = night_schedule(objects)
             logger.info('Created preliminary schedule')
+
             #Make plot of weather with obs times and save to plot folder
             if os.path.isdir(os.path.join(file_path,'plots')):
                 make_plot(objects, schedule,weather, pred_out = False, dir=os.path.join(file_path,'plots'))
@@ -65,6 +69,7 @@ if __name__ == '__main__':
                 os.mkdir(os.path.join(file_path,'plots'))
                 make_plot(objects, schedule,weather, pred_out = False, dir=os.path.join(file_path,'plots'))
             logger.info('Saved Plot of observing schedule')
+            
             #Creates file - #FIXME: Add relevant parameters to params
             create_ACP_scheudle(objects, schedule)
             logger.info('Created schedule for ACP')
@@ -73,9 +78,17 @@ if __name__ == '__main__':
         except (KeyboardInterrupt, Exception) as e:
             if e == KeyboardInterrupt:
                 print('Program terminated, remember to restart this script once you are done!')
+                logger.info('The program was interupted, remember to restart it!')
                 sys.exit(0)
             else:
                 print('There was an error in execution:\n', e)
+                logger.warning('There was an error in execution: \n', e)
+                passes += 1
+                if passes => 3:
+                    pass
+                else:
+                    logger.warning('The program terminated 3 times, scripts must be modified!')
+                    sys.exit(0)
                 #MAke some kind of message with bot or so
 
             #TODO: Add another exception for early termination due to other things and send email or so
