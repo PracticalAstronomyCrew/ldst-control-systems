906a0709dd36bcebf8000e1083bfb90585b3dd08
Naderschah <F.Semler@student.rug.nl>
2021-08-27 12:35:04 +0200

    third try

--- trials.py
+++ trials.py
@@ -1,3 +1,5 @@
+#!python
+#!C:\Users\blaau\AppData\Local\Programs\Python\Python39
 from fabric import Connection
 
 gate = Connection(host='kapteyn.astro.rug.nl', user='telescoop', forward_agent = True, connect_kwargs = {'allow_agent':True})
