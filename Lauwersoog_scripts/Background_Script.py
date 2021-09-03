71c91a2b8632fb0894e790abaa1b45964d2b69ed
Naderschah <F.Semler@student.rug.nl>
2021-09-03 21:33:57 +0200

    commit

--- Lauwersoog_scripts/Background_Script.py
+++ Lauwersoog_scripts/Background_Script.py
@@ -70,7 +70,7 @@ if __name__ == '__main__':
             #Move current config and database to backup folder
             #backup_config(file_path)
             #retrieve new config and database
-            get_config_database(file_path, remote_path) #FIXME: In case no config is found use backup
+            #get_config_database(file_path, remote_path) #FIXME: In case no config is found use backup
             #Init of scheduling takes care of the whole process from script start to sun rise
             Scheduling(file_path,remote_path) 
 
