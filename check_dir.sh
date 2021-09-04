6e5b2ec75668edffb617b85b720d40362333976b
Naderschah <F.Semler@student.rug.nl>
2021-09-04 12:39:07 +0200

    debugging-kapteyn

---
+++ check_dir.sh
@@ -0,0 +1,6 @@
+#!/bin/bash.sh
+dir_path = $1
+if [[ -d "$dir_path" ]]
+    then echo "1"
+    else echo "0" 
+fi
---
+++ check_file.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+file_path = $1
+if [[ -d "$file_path" ]]
+    then echo "1"
+    else echo "0" 
+fi
